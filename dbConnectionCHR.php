48148689ee1f2072738c125ca25e4c5401eb1e36
Marco Chalub <marco.chalub@gmail.com>
2019-05-08 14:52:08 +1000

    DB Connection chr DB

--- dbConnectionCHR.php
+++ dbConnectionCHR.php
@@ -2,7 +2,7 @@
  //Connect to database
  $USER     = "chr";
  $PASSWORD = "Pas4chr123!";
- $DBNAME   = "chrDB";
+ $DBNAME   = "chr";
  $conn = mysqli_connect("52.237.241.24", $USER, $PASSWORD, $DBNAME, 3306)
             or die("mySQL server connection failed");
 ?>
