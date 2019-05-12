2a3902b15c115cc4b8c22c8c80dfd024581eeba6
EdgarHernandez <edgarhrojas@hotmail.com>
2019-05-12 03:56:32 -0500

    Fix Menu

--- footer.php
+++ footer.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Caprivi Healthcare Research</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="js/bootstrap.min.js"></script>
-</head>
-<body>
-	<h6 align="center">Caprivi Healthcare Research</h6>
-</body>
-</html>
+<div align="center">
+<br/>
+<h6 align="center">Caprivi Healthcare Research</h6>
+</div>
--- menu.php
+++ menu.php
@@ -1,13 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Caprivi Healthcare Research</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <script src="js/bootstrap.min.js"></script>
-</head>
-<body>
-
-</body>
-</html>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<a class="navbar-brand" href="index.php"><img src="images/logo.png"/></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	<ul class="navbar-nav mr-auto">
+	  <li class="nav-item dropdown">
+		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		  Research
+		</a>
+		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		  <a class="dropdown-item" href="../content/CloudResearch.html">Cloud Research</a>
+		  <a class="dropdown-item" href="CloudComputing.html">What is Cloud Computing</a>
+		  <a class="dropdown-item" href="CloudTypes.html">Cloud Types</a>
+		  <div class="dropdown-divider"></div>
+		  <a class="dropdown-item" href="ResearchProject.html">Research Project</a>			  
+		</div>
+	  </li>
+	  <li class="nav-item dropdown">
+		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		  Get Involved
+		</a>
+		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		  <a class="dropdown-item" href="MakeAppointment.php">Make Appointment</a>
+		  <a class="dropdown-item" href="SubscribeNewsletter.php">Subscribe to Newsletter</a>
+		</div>
+	  </li>
+		<li class="nav-item">
+		<a class="nav-link" href="Login.html">Publications</a>
+		</li>
+	  <li class="nav-item dropdown">
+		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		  About CHR
+		</a>
+		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		  <a class="dropdown-item" href="AboutCHR.html">About CHR</a>
+		  <a class="dropdown-item" href="OurTeam.html">Our Team</a>	 
+		</div>
+	   </li>
+		<li class="nav-item">
+		<a class="nav-link" href="ContactUs.html">Contact Us</a>
+		</li>
+		<li class="nav-item">
+		<a class="nav-link" href="Login.html">Login</a>
+		</li>
+	</ul>
+	<form class="form-inline my-2 my-lg-0">
+	  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+	  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+	</form>
+  </div>
+</nav>
+	
