bbcaf3753461003a4a763d6139db7b76c77d09ae
Marco Chalub <marco.chalub@gmail.com>
2019-05-15 10:38:33 +1000

    Partial journals

--- web/journals/journals_delete.php
+++ web/journals/journals_delete.php
@@ -3,23 +3,35 @@
 <!-- Date: 02 May 2018 -->
 <!-- Software: Microsoft Visual Studio Code -->
 <!-- Platform: macOS Mojave 10.14.4 -->
-<!-- Purpose: Assessment 3 -->
+<!-- Purpose: Software Project -->
 <!-- *********************************************************************** -->
-<html>
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Marco's Library</title>
+    <meta charset="UTF-8">
+    <title>Caprivi Healthcare Research</title>
+	<link rel="stylesheet" href="../../css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="../../js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="../../js/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="../../js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>	
 </head>
-
 <body>
-    <h1>Delete Book</h1>
+<?php
+    include('../menu.php');
+    echo $_SESSION['userName'];
+?>
+    <h1>Delete Journal</h1>
     <?php
-        if (empty($_GET['book_id']))
-            die("You must select a book to delete");
-        $book_id = $_GET['book_id'];
+        if (empty($_GET['journal_id']))
+            die("You must select a journal to delete");
+        $journal_id = $_GET['journal_id'];
         //open the server connection
-        require 'dbConnectBooks.php'; 
+        require '../database/dbConnectionCHR.php'; 
         //delete the customer
-        $sql = "DELETE FROM Books WHERE BookID = $book_id";
+        $sql = "DELETE FROM journals WHERE jaournal_id = $journal_id";
         $result = mysqli_query($conn, $sql) or die("Error deleting record - ".mysqli_error($conn));
         $numrows = mysqli_affected_rows($conn);
         if ($numrows == 1)
@@ -31,5 +43,8 @@
     <br><br>
     <a href="booklist.php?book_type=A">Book List</a>
     <br><a href="newbook.html">New book</a>
-    </body>
+    <?php
+	    include('../footer.php');
+    ?>
+</body>
 </html>
