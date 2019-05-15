07c67d1beba3bd80d49e15941e5645918a848073
Marco Chalub <marco.chalub@gmail.com>
2019-05-15 12:34:49 +1000

    Journals database changes

--- journals/journals_list.php
+++ journals/journals_list.php
@@ -12,14 +12,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Caprivi Healthcare Research</title>
-	<link rel="stylesheet" href="../../css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="../css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<script src="../js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="../js/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="../js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>	
 </head>
 <body>
 <?php
-	include('../menu.php');
+	include('../main/menu.php');
 ?>
 
 <div class="container">
@@ -29,30 +29,17 @@
             die("You must log in!");
 
         //open db connection
-        require 'dbConnectionCHR.php';
+        require '../main/dbConnectionCHR.php';
 
-        //select book type description
-        $sql_btd = "SELECT bt.Description FROM journals jn
-        WHERE bt.Book_type = '".$book_type."'";
-
-        $result = mysqli_query($conn, $sql_btd) or die("Error reading book types ".mysqli_error($conn));
-
-        //build book type html
-        if ($book_type_desc = mysqli_fetch_array($result))
-            echo "Search Criteria: Book Type: $book_type_desc[Description]<br><br>";
-        
-        //back to search page
-        echo "<a href=marco.php>Search again</a><br><br>";
-
-        //select books
-        $sql = "SELECT b.BookID,
-                    b.ISBN,
-                    b.Title,
-                    b.Author,
-                    (SELECT bt.Description FROM BookTypes bt
-                        WHERE bt.Book_type = b.Booktype) AS Booktype,
-                    b.Price
-                    FROM Books b";
+        $sql = "SELECT j.Journal_Number,
+                    j.Journal_Name,
+                    (SELECT jt.Description FROM journal_types jt
+                        WHERE jt.Book_type = b.Booktype) AS Journal_Type,
+                    j.Publication_Date,
+                    (SELECT s.First_Name || ' ' || s.Surname FROM staff s
+                        WHERE s.Staff_Number = j.Staff_Number) AS Journal_Author
+                    FROM journals j
+                    WHERE j.Staff_Number = ".$_SESSION['userId '];
 
         //append where clause in case of book type is not All
         if ($book_type != 'A')
@@ -62,7 +49,7 @@
 
         //build result html
         echo "<table>";
-        echo "<tr><th>Book ID</th>
+        echo "<tr><th>Publication</th>
                     <th>ISBN</th>
                     <th>Title</th>
                     <th>Author</th>
@@ -97,7 +84,7 @@
     </div>	
 
 <?php
-	include('../footer.php');
+	include('../main/footer.php');
 ?>
 </body>
 </html>
--- main/menu.php
+++ main/menu.php
@@ -28,7 +28,7 @@
 		</div>
 	  </li>
 		<li class="nav-item">
-		<a class="nav-link" href="journals_list.php">Publications</a>
+		<a class="nav-link" href="../journals/journals_list.php">Publications</a>
 		</li>
 	  <li class="nav-item dropdown">
 		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
