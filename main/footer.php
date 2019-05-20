d31db0a2433c6585c9681f2a6e075d226f365bd0
Marco Chalub <marco.chalub@gmail.com>
2019-05-20 10:54:13 +1000

    Footer postion change to fixed

--- main/footer.php
+++ main/footer.php
@@ -3,10 +3,10 @@
 /* Pie de pagina*/
 #footer {
 	width: 100%;
-	height: 40px;
+	/*height: 40px;*/
 	background: #333;
 	border-top: 2px solid #000;
-	position: absolute;
+	position: fixed;
 	bottom: 0;
 	color:#FFFFFF;
 }
