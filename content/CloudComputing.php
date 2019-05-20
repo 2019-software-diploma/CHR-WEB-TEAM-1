b466fd99252d8d069eb7631fa209dc705d9afa37
HuiLu1121 <49064427+HuiLu1121@users.noreply.github.com>
2019-05-20 13:02:19 +1000

    v2

--- content/CloudComputing.php
+++ content/CloudComputing.php
@@ -23,10 +23,10 @@
 ?>
 	
 <div class="container">
-
-<p>Cloud computing is an innovative paradigm that provides users with on-demand access to a shared pool of configurable computing resources such as servers, storage, and applications. </p>
-<p>The term “cloud” is a metaphor for the internet. In the past, all of your applications and software had to be on a computer or server that you could only access at a specific location. With the introduction of the cloud, people could access their programs and information using the internet as the conduit. This principle also applies to data storage. Rather than keeping folders full of critical work on your computers and servers, that data can be stored remotely and backed up to the cloud.</p>
-<p>Cloud computing is changing the way doctors, nurses, clinics, and hospitals deliver quality, cost-effective services to their patients. Health provider can be better visualised when it comes to managing millions of electronic patient records, integrating social and health care information, and developing the infrastructure to connect the countless trusts, hospitals, surgeries, and clinics.</p>
+<p>What is cloud Computing</p>
+<p>Cloud computing is an innovative paradigm that provides users with on-demand access to a shared pool of configurable computing resources such as servers, storage, and applications. 
+   The term “cloud” is a metaphor for the internet. In the past, all of your applications and software had to be on a computer or server that you could only access at a specific location. With the introduction of the cloud, people could access their programs and information using the internet as the conduit. This principle also applies to data storage. Rather than keeping folders full of critical work on your computers and servers, that data can be stored remotely and backed up to the cloud.
+   Cloud computing is changing the way doctors, nurses, clinics, and hospitals deliver quality, cost-effective services to their patients. Health provider can be better visualised when it comes to managing millions of electronic patient records, integrating social and health care information, and developing the infrastructure to connect the countless trusts, hospitals, surgeries, and clinics.</p>
 
 
 </div>	
