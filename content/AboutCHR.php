b4ab431860a0b9493547da0b92f29e0f2343bc7d
HuiLu1121 <49064427+HuiLu1121@users.noreply.github.com>
2019-05-20 12:47:47 +1000

    Update AboutCHR.php
    content for about us

--- content/AboutCHR.php
+++ content/AboutCHR.php
@@ -24,7 +24,8 @@
 	
 <div class="container">
 
-About CHR
+<p>About CHR</p>
+<p>Caprivi  Healthcare  Research  (CHR)  is  a  non-profit  organisation.  The  CHR  specialises 1in   research,   designing   and   developing   Cloud   healthcare   solutions   in   developing countries,  particularly  for  remote  clinics.  The  CHR  have  been  delivering  Cloud  based healthcare  research  services  since  2017.  The  CHR  mission  is  to  research,  educate  and empower disadvantaged community with ICT Cloud solutions particularly in developing countries.  CHR  has  a  number  of  ICT  services  that  they  intent  to  incorporated  on  their website  (http://caprivihealthcareresearch.org/)  that  will  help  to  expand  their  business global operations.</p>
 
 </div>	
 <?php
