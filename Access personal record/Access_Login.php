c3d2bacdb10839e7d4f1986dad7314a9499b61c1
Dawoon <49132337+choidw0119@users.noreply.github.com>
2019-05-22 11:12:39 +1000

    Deleting Access personal record folder

--- Access personal record/Access_Login.php
+++
@@ -1,9 +0,0 @@
-<html>
-	 <h1>Do you want Access Personal Record?</h1>
-	 <h2>Please input Firstname & PW</h2>
-	 <form name="accessform" id="accessform" method="POST" action="validate.php" >
-		 <label for="firstname">Firstname:</label><input type="text" name="firstname"><br><br>
-		 <label for="password">Password:</label><input type="password" name="password"><br><br>
-		 <input type="submit" value="log in">
-	 </form>
-</html>
