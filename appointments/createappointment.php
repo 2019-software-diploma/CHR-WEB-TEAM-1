17f204461aee058c25d98bbd7388d7c7187735e0
dbaldwin2019 <49132303+dbaldwin2019@users.noreply.github.com>
2019-05-22 10:13:26 +1000

    Create Appointments
    Create Appointments

---
+++ appointments/createappointment.php
@@ -0,0 +1,40 @@
+<?php
+require 'dbConnectCHR.php'; 
+	$First_Name = $_POST['First_Name'];
+    $Surname = $_POST['Surname'];
+    $Phone_Number = $_POST['Phone_Number'];
+	$StaffID = $_POST['StaffID'];
+    $Date = strtotime($_POST['Date']);
+	$NewDate = date('Y/m/d',$Date);
+	$Time = $_POST['Time'];
+	$Summary = $_POST['Summary'];
+	
+	
+	$sql = "INSERT INTO `Appointments` VALUES (DEFAULT, '$First_Name', '$Surname', '$Phone_Number', '$StaffID', '$Time', '$NewDate', '$Summary')";
+	echo $NewDate;
+	echo $Time;
+	echo $sql;
+?>
+<html>
+	<head>
+		<title>Make Appointment</title>
+	</head>
+	<body>
+		<h1>Make Appointment</h1>
+	<?php
+	
+	if (mysqli_query($conn, $sql))
+	{
+		if (mysqli_affected_rows($conn) == 1) 
+		{
+			echo "<p>Appointment made</p>";
+		}
+    } 
+	else 
+	{
+        echo "<p>Error: " . mysqli_error($conn);
+    }
+	mysqli_close($conn);
+	?>
+	</body>
+</html>
