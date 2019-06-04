a80d5db86e762b964a060c121a09e20935ba0440
EdgarHernandez <edgarhrojas@hotmail.com>
2019-06-04 11:25:15 -0500

    Fix footer

--- main/footer.php
+++ main/footer.php
@@ -5,7 +5,21 @@
  * Purpose: Footer page of the Web Portal
  */
  ?>
-
+<style type="text/css">
+<!--
+/* Pie de pagina*/
+#footer {
+	border-top: 0px solid #000;
+    position:fixed;
+    left:0px;
+    bottom:0px;
+    width:100%;
+    height:120px;
+    background-color: #3597D4;
+    color: #ffffff;
+}
+-->
+</style>
 <footer class="container-fluid bg-4 text-center" id="footer">
     <p>
     <div class="row">
@@ -43,40 +57,4 @@
           <li><a class="text-secondary small" href="../content/ContactUs.php">Contact US</a></li>
         </ul>
       </div>
-
-<footer class="bg-mine" id="footer">
-    <div class="container">
-        <div class="row text-center text-xs-center text-sm-left text-md-left">
-            <div class="col-xs-12 col-sm-3 col-md-3">
-                <small>Research</small>
-				<ul class="list-unstyled quick-links">
-                    <li><p class="small"><a href="../content/CloudResearch.php">Cloud Research</a></p></li>
-                    <li><p class="small"><a href="../content/CloudComputing.php">What is Cloud Computing</a></p></li>
-                    <li><p class="small"><a href="../content/CloudTypes.php">Cloud Type</a></p></li>
-                    <li><p class="small"><a href="../content/ResearchProject.php">Research Project</a></p></li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-3 col-md-3">
-                <small>Get Involved</small>
-                <ul class="list-unstyled quick-links">
-				<li><p class="small"><a href="../subscribe/subscribe_news.php">Subscribe to Newsletter</a></p></li>
-				<li><p class="small"><a href="../appointments/appointment_new.php">Make Appointment</a></p></li>                    
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-3 col-md-3">
-                <small>Out ICT Solutions</small>
-                <ul class="list-unstyled quick-links">
-                <li><p class="small"><a href="../journals/journals_list.php">Publications</a></p></li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-3 col-md-3">
-                <small>Quick links</small>
-                <ul class="list-unstyled quick-links">
-                    <li><p class="small"><a href="../content/AboutCHR.php" >About CHR</a></p></li>
-                    <li><p class="small"><a href="../content/OurTeam.php">Our Team</a></p></li>
-                    <li><p class="small"><a href="../content/ContactUs.php">Contact Us</a></p></li>
-                </ul>
-            </div>
-        </div>
-    </div>
   </footer>
