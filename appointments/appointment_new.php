711729ee36ced206068849adc38552497f49de1b
dbaldwin2019 <49132303+dbaldwin2019@users.noreply.github.com>
2019-06-14 12:31:41 +1000

    Bootstrap stuff

--- appointments/appointment_new.php
+++ appointments/appointment_new.php
@@ -35,19 +35,44 @@ require '../main/dbConnectionCHR.php';
 <?php
 	include('../main/menu.php');
 ?>
-	<h1>Make Appointment</h1>
-	
-		<form action="appointment_insert.php" method="post">
+<div class = "container">
+	<div class="jumbotron small">
+		<h1>Make Appointment</h1>
+	</div>
+		<form action="appointment_insert.php" method="post" novalidate>
 		
 		
-		<table border="0">
-			<tr><td><p>First Name:<br /><input name="First_Name" type="text"></p></td>
-			<td><p>Surname:<br /><input name="Surname" type="text"></p></td>
-			<td><p>Phone Number:<br /><input name="Phone_Number" type="text"></p></td></tr>
+			<div class="row">
+				<div class="form-group col"><p>First Name:<br />
+					<input class="form-control" name="First_Name" type="text" required>
+					<div class="valid-feedback">Valid.</div>
+					<div class="invalid-feedback">Please fill out this field.</div>
+					</p></div>
+				<div class="form-group col"><p>Surname:<br />
+					<input class="form-control" name="Surname" type="text" required>
+					<div class="valid-feedback">Valid.</div>
+					<div class="invalid-feedback">Please fill out this field.</div>
+					</p></div>
+				<div class="form-group col"><p>Phone Number:<br />
+					<input class="form-control" name="Phone_Number" type="text" required>
+					<div class="valid-feedback">Valid.</div>
+					<div class="invalid-feedback">Please fill out this field.</div>
+					</p></div>
+			</div>
 			
-			<tr><td colspan="3" ><p>Reason For Appointment:<br /><input name="Summary"  maxlength="500" height="50" width="100"></td></tr>
+			<div class="row">
+				<div class="form-group col">
+					<label for="Summary">Reason For Appointment:</label>
+					<textarea class="form-control" id="Summary" name="Summary" rows="3" ></textarea>
+				</div>
+			</div>
 			
-			<tr><td><p>Staff No:<br /><select name="StaffID" >
+			
+			<div class="row">
+				<div class="form-group col">
+					<label for="StaffID">Staff No:</label>
+					<select class="form-control" name="StaffID" id="StaffID" >
+					
 			
 			<?php
 	
@@ -67,33 +92,39 @@ require '../main/dbConnectionCHR.php';
 				
 			 mysqli_close($conn);
 			?>
-			</select></p></td>
+					</select>
+				</div>
+			</div>
 			
-			
-			<td><p>Date:<br /><input type="text" name="Date" id="Date"></p></td>
-			<td><p>Time:<br /><select name="Time">
-					<option value="9:00">9:00am</option>
-					<option value="9:30">9:30am</option>
-					<option value="10:00">10:00am</option>
-					<option value="10:30">10:30am</option>
-					<option value="11:00">11:00am</option>
-					<option value="11:30">11:30am</option>
-					<option value="13:00">1:00pm</option>
-					<option value="13:30">1:30pm</option>
-					<option value="14:00">2:00pm</option>
-					<option value="14:30">2:30pm</option>
-					<option value="15:00">3:00pm</option>
-					<option value="15:30">3:30pm</option>
-					<option value="16:00">4:00pm</option>
-					<option value="16:30">4:30pm</option>
-					
-				</select></p></td>
-			</tr>
+			<div class="row">
+				<div class="form-group col">
+					<label for="Date">Date:</label>
+					<input class="form-control" type="text" name="Date" id="Date"></p>
+				</div>
+				<div class="form-group col">
+					<label for="Time">Date:</label>
+					<select class="form-control" name="Time" id="Time">
+						<option value="9:00">9:00am</option>
+						<option value="9:30">9:30am</option>
+						<option value="10:00">10:00am</option>
+						<option value="10:30">10:30am</option>
+						<option value="11:00">11:00am</option>
+						<option value="11:30">11:30am</option>
+						<option value="13:00">1:00pm</option>
+						<option value="13:30">1:30pm</option>
+						<option value="14:00">2:00pm</option>
+						<option value="14:30">2:30pm</option>
+						<option value="15:00">3:00pm</option>
+						<option value="15:30">3:30pm</option>
+						<option value="16:00">4:00pm</option>
+						<option value="16:30">4:30pm</option>
+						
+					</select></div>
+			</div>
 
-
-		</table>
-		<input type="submit" value="Create Appointment">
+		<button type="submit" class="btn-primary btn-sm">Create Appointment</button>
 		</form>
+</div>
 <?php
 	include('../main/footer.php');
 ?> 
